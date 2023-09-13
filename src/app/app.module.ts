335c64106ab3e8318a8d351cb20bdf808a182d4d
Guizemoreira <guisouza.moreira@gmail.com>
2023-09-13 17:45:51 +0000

    Add task components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TaskListComponent } from './tasks/task-list/task-list.component';
+import { TaskListItemComponent } from './tasks/task-list-item/task-list-item.component';
+import { TaskFormComponent } from './tasks/task-form/task-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    TaskListItemComponent,
+    TaskFormComponent
 
   ],
   imports: [
